48c9201c3372e0b8901333b306a8d9ea1ef41e86
Daryl Rogony <darylrogony@gmail.com>
2022-10-11 15:11:49 +0300

    Fit: generate models and controllers

---
+++ db/migrate/20221011120753_create_orders.rb
@@ -0,0 +1,13 @@
+class CreateOrders < ActiveRecord::Migration[7.0]
+  def change
+    create_table :orders do |t|
+      t.string :order_id
+      t.string :status
+      t.datetime :date
+      t.string :cusomer_id
+      t.string :product_id
+
+      t.timestamps
+    end
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Customer.create(name: "John", address: "452 Georgia", id_no: "4232222", mobile_no: "75267362", city: "Geotgia")
+Customer.create(name: "Daniele", address: "452 Georgia", id_no: "4232222", mobile_no: "75267362", city: "Geotgia")
+Customer.create(name: "Peter", address: "452 Georgia", id_no: "4232222", mobile_no: "75267362", city: "Geotgia")
+Customer.create(name: "John", address: "452 Georgia", id_no: "4232222", mobile_no: "75267362", city: "Geotgia")
+Customer.create(name: "Debby", address: "452 Georgia", id_no: "4232222", mobile_no: "75267362", city: "Geotgia")

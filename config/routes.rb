914b5014472531a87924d51f35b03d525e90fcb6
Daryl Rogony <darylrogony@gmail.com>
2022-10-11 18:20:40 +0300

    FIt: add model associations

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   # root "articles#index"
 
   resources :customers, only: [:index, :show]
-  resources :orders, only: [:index]
+  resources :orders, only: [:index, :show, :update]
 end

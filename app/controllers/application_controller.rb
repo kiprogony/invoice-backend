8cf3f316adeda9fdf8cef0fed9f00c7d45aa7e52
Daryl Rogony <darylrogony@gmail.com>
2022-10-12 18:57:17 +0300

    Fit: update the applictation controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,20 @@
 class ApplicationController < ActionController::API
-    include ActionController::Cookies
+#     include ActionController::Cookies
 
   
 
-rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
-  before_action :authorize
+# rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity
+#   before_action :authorize
 
-  private
+#   private
 
-  def authorize
-    @current_user = User.find_by(id: session[:user_id])
-    render json: { errors: ["Not authorized"] }, status: :unauthorized unless @current_user
-  end
+#   def authorize
+#     @current_user = User.find_by(id: session[:user_id])
+#     render json: { errors: ["Sorry you are not authorized"] }, status: :unauthorized unless @current_user
+#   end
 
-  def render_unprocessable_entity(exception)
-    render json: { errors: exception.record.errors.full_messages }, status: :unprocessable_entity
-  end
+#   def render_unprocessable_entity(exception)
+#     render json: { errors: exception.record.errors.full_messages }, status: :unprocessable_entity
+#   end
 
 end
